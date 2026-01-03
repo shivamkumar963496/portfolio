2f82aba545ae3101c4335ab48332cf891390d152
shivamkumar963496 <shivamkumar963496@gmail.com>
2026-01-03 09:21:02 +0530

    Add files via upload

---
+++ linkhub/c-programs/loops/for-loops/programs/cp1.c
@@ -0,0 +1,17 @@
+//Print hello world ‘n’ times. Take ‘n’ as input from user
+
+#include<stdio.h>
+int main()
+{
+int n;
+printf("enter the number of n=");
+scanf("%d",&n);
+for (int i = 1; i <=n; i++)
+{
+    printf("hellow shivam\n");
+}
+
+
+
+    return 0;
+}

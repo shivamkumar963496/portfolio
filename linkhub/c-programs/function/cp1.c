1de44d0646ed99eba37ad44540bf6430a6d557bd
shivamkumar963496 <shivamkumar963496@gmail.com>
2026-01-09 19:35:31 +0530

    Update cp1.c

--- linkhub/c-programs/function/cp1.c
+++ linkhub/c-programs/function/cp1.c
@@ -1,26 +1,29 @@
-/*Program 1: Addition of Two Numbers (Function)*/
+/* Program 1: Addition of Two Numbers using Function
+   Ye program function ka use karke do numbers ka addition karta hai
+*/
 
+#include <stdio.h>   // Standard input-output header file
+                    // Input aur output ke liye header file
 
-#include <stdio.h>
-
-// function definition
-// यह function दो numbers को जोड़कर result return करता है
+// Function definition
+// Ye function do numbers leta hai aur unka sum return karta hai
 int add(int a, int b)
 {
-    // a और b का sum निकालना
+    // a aur b ka addition kar rahe hain
     int sum = a + b;
 
-    // result वापस भेजना
+    // calculated sum wapas bhej rahe hain
     return sum;
 }
 
 int main()
 {
-    // function call
+    // Function call: add function ko 10 aur 20 pass kiya
     int result = add(10, 20);
 
-    // output print
+    // Final result print kar rahe hain
     printf("Sum = %d", result);
 
+    // Program yahin end hota hai
     return 0;
 }

46eccfc64a6f0c49d20a23f7b6971d4839bfcee2
shivamkumar963496 <shivamkumar963496@gmail.com>
2026-01-09 19:38:44 +0530

    Update cp2.c

--- linkhub/c-programs/function/cp2.c
+++ linkhub/c-programs/function/cp2.c
@@ -1,20 +1,28 @@
-/*Program 2: Even or Odd using Function*/
-#include <stdio.h>
+/* Program 2: Even or Odd using Function
+   Ye program function ka use karke check karta hai
+   ki number even hai ya odd
+*/
 
-// यह function check करता है कि number even है या odd
+#include <stdio.h>   // Input-output ke liye header file
+
+// Ye function check karta hai ki number even hai ya odd
 void evenOdd(int n)
 {
-    // अगर remainder 0 है तो even
+    // Agar number ko 2 se divide karne par remainder 0 aaye
+    // to number Even hota hai
     if(n % 2 == 0)
         printf("Number is Even");
     else
+        // Agar remainder 0 nahi aaya to number Odd hota hai
         printf("Number is Odd");
 }
 
 int main()
 {
-    // function call
+    // Function call:
+    // 7 ko evenOdd function me pass kar rahe hain
     evenOdd(7);
 
+    // Program yahin end hota hai
     return 0;
 }

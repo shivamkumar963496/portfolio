36e51b74abc7bb974622128d5e0ad2b68c17d072
shivamkumar963496 <shivamkumar963496@gmail.com>
2026-02-03 20:20:11 +0530

    Add python12.html to chapter 1

--- linkhub/python/chapter-1/python12.py
+++
@@ -1,7 +0,0 @@
-# Take Celsius input and calculate Fahrenheit value
-
-celsius = float(input("ENTER CELSIUS VALUE = "))
-
-fahrenheit = (celsius * 9 / 5) + 32
-
-print("THE FAHRENHEIT VALUE IS =", fahrenheit)

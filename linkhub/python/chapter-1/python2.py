66bb5c28294e1944e7b02dde5a752971b7d01635
shivamkumar963496 <shivamkumar963496@gmail.com>
2026-02-03 20:11:58 +0530

    Add HTML representation of Python Program 2
    Converted Python code into an HTML format with styling.

--- linkhub/python/chapter-1/python2.py
+++
@@ -1,6 +0,0 @@
-a=15
-b=12
-print("THE SUM OF =", "A", "+" ,"B", ":", a+b)
-print("THE SUB OF =", "A", "-" ,"B", ":", a-b)
-print("THE MUL OF =", "A", "*" ,"B", ":", a*b)
-print("THE DIV OF =", "A", "/" ,"B", ":", a/b)

212bacc4f40ac2e5f2b4bca2de17cb7eb5ffe89d
shivamkumar963496 <shivamkumar963496@gmail.com>
2026-02-05 21:24:27 +0530

    Update and rename python13.py to python13.html

--- linkhub/python/chapter-3/python13.py
+++
@@ -1,9 +0,0 @@
-user=input("ENTER YOUR USER NAME = ")
-password=int(input("ENTER YOUR PASSWORD = "))
-
-
-if user=="shivam" and password==1234:
-    print("LOGIN SUCCESS")
-
-else:
-    print("INVALID DETAILS")
